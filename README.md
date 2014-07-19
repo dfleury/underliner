782b163ed79d00b58f696af0182d16950e592fd1
Diego Fleury <diego.fleury@corp.globo.com>
2014-07-19 16:36:36 -0300

    Implemented the two-way replacer

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-underliner
+Underliner
 ==========
 
-A space to underline converter plugin for Sublime Text
+This is a *space to underline* converter plugin for Sublime Text 3
+
+Motivation
+----------
+
+I figured out how boring is write python methods to describe a test case typing
+underlines (also called underscore) instead of spaces.
+So I made this plugin to replace spaces contained in selected code chunks in a
+practical way.
+
+Usage example
+-------------
+
+```
+test if can sum two numbers
+more   than  one spaces
+```
+
+After selecting a text like that above, there are many ways to activate the
+Underliner command such: context menu, main menu (`Edit > Convert Case >
+Underliner`), command list (`[Ctrl/Cmd]+Shift+P > Convert Case: Underliner`)
+and the keyboard shortcut (`[Ctrl/Cmd]+Shift+U`), which is my preferred option.
+The expected effect is the same text with each space character replaced to a
+underline:
+
+```
+test_if_can_sum_two_numbers
+more___than__one_spaces
+```
+You can do the reverse too. Same principle: select and execute the command.
+Spaces and underlines are counted and the most found charactere will be used as
+target to replace, maintaining the less found characteres.
+
+License
+-------
+
+The MIT License (MIT) - Copyright (c) 2014 Diego Fleury
--- Underliner.py
+++ Underliner.py
@@ -1,11 +1,42 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
 
 class UnderlinerCommand(sublime_plugin.TextCommand):
+
+    replacements = {
+        ' ': '_',
+        '_': ' ',
+    }
+
+    names = {
+        ' ': 'spaces',
+        '_': 'underlines',
+    }
+
     def run(self, edit):
         sels = self.view.sel()
+        replace_char = self.get_prevailing_char(sels)
         for sel in sels:
             text = self.view.substr(sel)
-            new_text = text.replace(' ', '_')
+            new_text = text.replace(
+                replace_char,
+                self.replacements[replace_char])
             self.view.replace(edit, sel, new_text)
-        sublime.status_message('Underlined successfully')
+
+        replaced_char = self.names[replace_char]
+        unreplaced_chard = self.names[self.replacements[replace_char]]
+        message = 'It was found more %s than %s, so it was replaced'
+        sublime.status_message(message % (replaced_char, unreplaced_chard))
+
+    def get_prevailing_char(self, sels):
+        '''decide which char will be replaced'''
+        all_text = ''
+        for sel in sels:
+            all_text += self.view.substr(sel)
+        spaces_count = all_text.count(' ')
+        underlines_count = all_text.count('_')
+        if spaces_count > underlines_count:
+            return ' '
+        else:
+            return '_'
